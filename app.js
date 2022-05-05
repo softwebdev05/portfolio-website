bf99eb2e099f44e581599b2113eae6165a0f1c9e
softwebdev05 <softwebdev05@gmail.com>
2022-05-05 15:04:37 +0300

    created skills section

--- app.js
+++ app.js
@@ -7,3 +7,23 @@ setTimeout(() => {
     carousels[0].classList.add('active');
     setIntervalForSlider();
 }, 3500);
+
+//toggle button
+const toggleBtn = document.querySelector('.toggle-btn');
+const linkContainer = document.querySelector('.links-container');
+
+toggleBtn.addEventListener('click', () => {
+    toggleBtn.classList.toggle('active');
+    linkContainer.classList.toggle('show');
+})
+
+// links
+
+const links = document.querySelectorAll('.link');
+
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        links.forEach(ele => ele.classList.remove('active'));
+        link.classList.add('active');
+    })
+})
